3f01c1113419c1b7efbe0c82198fb0d4f0a9af2b
RonnyRitscher <r.k.ritscher@web.de>
2019-03-06 08:44:09 +0100

    2019.03.06_08:45

--- app/src/main/java/de/proneucon/mygooglemapdemo/MainActivity.java
+++ app/src/main/java/de/proneucon/mygooglemapdemo/MainActivity.java
@@ -1,7 +1,9 @@
 package de.proneucon.mygooglemapdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //ButtonClick toMap
+    public void toMap(View view) {
+        Intent intent = new Intent(this , MapsActivity.class);
+        startActivity(intent);
     }
 }
